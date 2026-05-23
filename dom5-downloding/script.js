58ce36f9d557f05f16c8a7f0620bb2c038f15d9d
Shivam-136 <shivam243028@gmail.com>
2026-05-23 23:46:57 +0530

    Downloding-animation

---
+++ dom5-downloding/script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector("#download");
+let growth = document.querySelector("#growth");
+let percent = document.querySelector("#percent");
+
+btn.addEventListener("click", function(){
+
+let count = 0;
+
+let interval = setInterval(function(){
+
+count++;
+
+growth.style.width = count + "%";
+
+percent.innerHTML = count + "%";
+
+if(count === 100){
+
+clearInterval(interval);
+
+btn.innerHTML = "Completed";
+
+}
+
+},50);
+
+});
